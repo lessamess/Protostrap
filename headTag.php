141cca7e9e881b625425272b856a36c0b8d80c9a
Memi Beltrame <memi.beltrame@liip.ch>
2012-09-11 11:23:43 +0200

    better viewport handling

--- headTag.php
+++ headTag.php
@@ -1,11 +1,12 @@
     <meta charset="utf-8"><meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
     <!-- Le styles -->
     <link href="./assets/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/add2home.css">
     <link href="./assets/css/protostrap.css" rel="stylesheet">
+    <link href="./assets/css/main.css" rel="stylesheet">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
