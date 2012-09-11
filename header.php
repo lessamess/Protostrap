85284b1df32517b6b38ff0e5a6df58da6255a677
Memi Beltrame <memi.beltrame@liip.ch>
2012-09-11 11:24:04 +0200

    logn/logout place

--- header.php
+++ header.php
@@ -1,3 +1,31 @@
-<!-- The following generates a placeholder svg image -->
-<img data-ph="160:60:Logo:fixedsize" />
-<br>
+
+<div class="row">
+            <span class="pile200 left">
+                <!-- The following generates a placeholder svg image -->
+                <img data-ph="160:60:Logo:fixedsize" />
+            </span>
+            <span class="pile80 ">
+                <?php if (!empty($loggedIn)){ ?>
+                <!-- user is Logged in -->
+                    <ul class="nav nav-pills pull-right">
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="icon-user"></i> <b class="caret"></b>
+                            </a>
+                            <ul class="dropdown-menu pull-right" style="right:0">
+                                <li>
+                                    <form action="" method="post">
+                                        <input type="hidden" name="logout" value="logout">
+                                        <button type="submit" name="" class="btn btn-primary">Logout</button>
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                 <?php } else {
+                    ?>
+                <!--  User is not logged in -->
+                    <a href="./account.php" class="" >Login</a>
+                <?php }?>
+            </span>
+</div>
