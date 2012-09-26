e96f84d0af705fb497245e4e7aeea54ca6147eea
Memi Beltrame <memi.beltrame@liip.ch>
2012-09-26 13:51:05 +0200

    add speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,12 @@
     $(function(){
-      // Some Protostrap goodies
-          // Generate Placeholder images
-          $('img[data-ph]').tsPlaceHold();
+        // Some Protostrap goodies
+        // Generate Placeholder images
+        $('img[data-ph]').tsPlaceHold();
+
+        // initialize fastclick
+        window.addEventListener('load', function() {
+            new FastClick(document.body);
+        }, false);
 
       // PUT YOUR JAVASCRIPT HERE BELOW
     })
--- protostrap_javascripts.php
+++ protostrap_javascripts.php
@@ -25,5 +25,6 @@
     <script src="./assets/js/bootstrap.min.js"></script>
     <script src="./assets/js/jquery.svg.placeholder.js"></script>
     <script src="./assets/js/add2home.js"></script>
+    <script type='application/javascript' src='./assets/js/fastclick.js'></script>
     <!-- You can add your code in this file -->
     <script src="./assets/js/main.js"></script>
