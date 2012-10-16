ffb515610b7d1c66def97dad96c578bc99626376
Memi Beltrame <memi.beltrame@liip.ch>
2012-10-16 10:59:02 +0200

    add examples

--- two.php
+++ two.php
@@ -51,36 +51,96 @@ include('./header.php');?>
                     <li class="active">Everest</li>
             </ul>
         </div>
+        <h1> Handy Snippets</h1>
         <h3>Accordion Using uniqId</h3>
         <div id="accordion1" class="accordion accordeon-wedge">
-                        <div class="accordion-group">
-                            <div class="accordion-heading">
-                                <a href="#<?php echo getUniqid();?>" data-parent="#accordion1" data-toggle="collapse" class="accordion-toggle">
-                                     One
-                                </a>
-                            </div>
-                            <div class="accordion-body collapse" id="<?php echo $lastUniqid;?>">
-                                <div class="accordion-inner">
-                                    <b>One open</b><br>
-                                    Id: <?php echo $lastUniqid;?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-group">
-                            <div class="accordion-heading">
-                                <a href="#<?php echo getUniqid();?>" data-parent="#accordion1" data-toggle="collapse" class="accordion-toggle">
-                                     Two
-                                </a>
-                            </div>
-                            <div class="accordion-body collapse" id="<?php echo $lastUniqid;?>">
-                                <div class="accordion-inner">
-                                    <b>Two open</b><br>
-                                    Id: <?php echo $lastUniqid;?>
-                                </div>
-                            </div>
-                        </div>
+            <div class="accordion-group">
+                <div class="accordion-heading">
+                    <a href="#<?php echo getUniqid();?>" data-parent="#accordion1" data-toggle="collapse" class="accordion-toggle">
+                         One
+                    </a>
+                </div>
+                <div class="accordion-body collapse" id="<?php echo $lastUniqid;?>">
+                    <div class="accordion-inner">
+                        <b>One open</b><br>
+                        Id: <?php echo $lastUniqid;?>
                     </div>
+                </div>
+            </div>
+            <div class="accordion-group">
+                <div class="accordion-heading">
+                    <a href="#<?php echo getUniqid();?>" data-parent="#accordion1" data-toggle="collapse" class="accordion-toggle">
+                         Two
+                    </a>
+                </div>
+                <div class="accordion-body collapse" id="<?php echo $lastUniqid;?>">
+                    <div class="accordion-inner">
+                        <b>Two open</b><br>
+                        Id: <?php echo $lastUniqid;?>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <h3>Grid Row 12 cols</h3>
+        <div class="row">
+            <span class="span4">
+                Span 4
+            </span>
+            <span class="span2">
+                Span 2
+            </span>
+            <span class="span6">
+                Span 6
+            </span>
+        </div>
+
+        <h3>Stacked Navigation with Wedge</h3>
+        <ul class="nav nav-tabs nav-stacked nav-wedge">
+            <li><a>foo</a></li>
+            <li><a>bar</a></li>
+            <li><a>baz</a></li>
+        </ul>
+
+        <h3>Stacks</h3>
+        <ul class="stacks">
+            <li >
+                <a >
+                    one
+                </a>
+            </li>
+            <li>
+                <a class="">
+                    two
+                </a>
+            </li>
+        </ul>
+
+        <h3>Tabs</h3>
+        <!-- ** TABS ** -->
+        <div class="tabbable">
+            <ul class="nav nav-tabs">
+                <li class="active"><a href="#tab1" data-toggle="tab">ONE</a></li>
+                <li><a href="#tab2" data-toggle="tab">TWO</a></li>
+                <li><a href="#tab3" data-toggle="tab">THREE</a></li>
+                <li><a href="#tab4" data-toggle="tab">FOUR</a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active" id="tab1">
+                    one
+                </div>
+                <div class="tab-pane " id="tab2">
+                    two
+                </div>
+                <div class="tab-pane " id="tab3">
+                    three
+                </div>
+                <div class="tab-pane " id="tab4">
+                    four
+                </div>
+            </div>
+        </div>
+        <!-- ** End of TABS **-->
 
 
       <hr>
