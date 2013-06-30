c92273bbfd61e6cc0ddc7863480f9dddb41d1440
Memi Beltrame <memi@lessamess.ch>
2013-06-30 21:21:44 +0200

    parse data

---
+++ core/dataParse.php
@@ -0,0 +1,131 @@
+<?php
+
+// Leave the code below alone
+if(isset($_SESSION['application'])){
+    $parsed = $_SESSION;
+    $setsession = false;
+} else {
+
+    $yaml = "";
+    // READ ALL FILES IN ASSETS/DATA AND CONCATENATE
+
+        //Open directory
+        $path = "assets/data/";
+        $dir = dir($path);
+
+        //List files in directory
+        while (($file = $dir->read()) !== false){
+
+            //Make sure it's a .txt file
+            if(strlen($file) <  5 or $file == ".DS_Store")
+                continue;
+            $yaml .= file_get_contents($path.$file);
+            $yaml .= "\n";
+        }
+
+        $dir->close();
+
+    $parsed = Spyc::YAMLLoad($yaml);
+    $_SESSION = $parsed;
+}
+
+// Handle datachanges
+
+if (!empty($_POST['session'])){
+
+    foreach($_POST['session'] as $key => $postSession) {
+        setSessionVar($key, $postSession);
+    }
+
+    $parsed = $_SESSION;
+
+}
+
+
+
+// Expose each session key as variable and handle livesearch entry
+$livesearch = "";
+$livesearchKeys = array("name", "title");
+
+foreach ($parsed as $key => $item){
+    $$key = $item;
+
+    if (is_array($$key)) {
+        $url="";
+        foreach($$key as $subkey => $var){
+            if($subkey === "livesearch") {
+                $url = ${$key}[$subkey]['url'];
+                unset(${$key}[$subkey]);
+            } else {
+                if(is_array($var)){
+                    ${$key}[$subkey]['id'] = $subkey;
+                    foreach ($livesearchKeys as $lsKey){
+                        if(!empty(${$key}[$subkey][$lsKey])){
+                            $livesearch .= "{value: '". addslashes(${$key}[$subkey][$lsKey]) . "', url: '" . $url ."?id=" . $subkey . "'},";
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
+$livesearch = substr($livesearch, 0, -1);
+
+
+function setSessionVar($key, $item){
+    $el = explode('.', $key);
+    $type = "default";
+    if(preg_match('/arrayPush/', $item)){
+        $type = "push";
+        $item = substr($item, 10);
+    }
+    switch($type){
+        case 'push':
+            switch(count($el)){
+                case 1:
+                    array_push($_SESSION[$el[0]], $item);
+                    break;
+                case 2:
+                    array_push($_SESSION[$el[0]] [$el[1]], $item);
+                    break;
+                case 3:
+                    array_push($_SESSION[$el[0]][$el[1]][$el[2]], $item);
+                    break;
+                case 4:
+                    array_push($_SESSION[$el[0]][$el[1]][$el[2]][$el[3]], $item);
+                    break;
+                case 5:
+                    array_push($_SESSION[$el[0]][$el[1]][$el[2]][$el[3]][$el[4]], $item);
+                    break;
+                case 6:
+                    array_push($_SESSION[$el[0]][$el[1]][$el[2]][$el[3]][$el[4]][$el[5]], $item);
+                    break;
+            }
+            break;
+        default:
+            switch(count($el)){
+                case 1:
+                    $_SESSION[$el[0]] = $item;
+                    break;
+                case 2:
+                    $_SESSION[$el[0]] [$el[1]]= $item;
+                    break;
+                case 3:
+                    $_SESSION[$el[0]][$el[1]][$el[2]] = $item;
+                    break;
+                case 4:
+                    $_SESSION[$el[0]][$el[1]][$el[2]][$el[3]] = $item;
+                    break;
+                case 5:
+                    $_SESSION[$el[0]][$el[1]][$el[2]][$el[3]][$el[4]] = $item;
+                    break;
+                case 6:
+                    $_SESSION[$el[0]][$el[1]][$el[2]][$el[3]][$el[4]][$el[5]] = $item;
+                    break;
+            }
+            break;
+    }
+
+
+}
