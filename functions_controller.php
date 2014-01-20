63b03c4af900e6129e4c54e8e6bc396a11e16ffe
Memi Beltrame <memi@lessamess.ch>
2014-01-20 18:02:39 +0100

    Add sublime snippets for fast development

--- functions_controller.php
+++ functions_controller.php
@@ -1,12 +1,14 @@
 <?php
 
-function numberFormat($num, $dez = 2){
+function figureFormat($num, $dez = 2){
     $num = floatval($num);
    return number_format($num, $dez, '.', '&rsquo;');
 }
 
+// Order stuff via uasort($array, 'itemOrder');
 function itemOrder($a, $b) {
 
-   
-    return $a['endDatumSortable'] > $b['endDatumSortable'] ? 1 : -1;
+    // Change 'orderKey' to whatever Index your array should be ordered by
+    return $a['orderIndex'] > $b['orderIndex'] ? 1 : -1;
+    
 }
