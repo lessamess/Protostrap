1e0daf4e0fc703d3ad598d436859ceab70c67480
Memi Beltrame <memi@lessamess.ch>
2014-04-15 16:31:34 +0200

    file not found file

--- fileNotFound.php
+++ fileNotFound.php
@@ -19,31 +19,29 @@ include('core/protostrap.php');
 <?php
 // this includes all the markup that loads css files and similar stuff,
 // if you have to add more css, that's the place to do it.
-include('./headTag.php');?>
+include('./meta_headTag.php');?>
 
   </head>
 
-  <body>
+  <body  class="header-fixed">
 <?php
-        // *** STATIC TOP NAVBAR ***
-        // This defines which navigation item is active. each pair of quotes corressponds to an item
-        // DO NOT REMOVE
-        $navbarClasses = Array('','','','','','','','',''); 
+        $activeNavigation = "one";
+
         // this includes the markup for a static top navbar. Remove the // to include.
-        include('./navBarStaticTop.php');
-        
-        
+        include('./snippets/navBarTop.php');
+
+
         // *** iOS TAB-BAR ***
         // This defines which tab is active. each pair of quotes corressponds to a tab
         // DO NOT REMOVE
         $tabClasses = Array('active','','','','');
         // this includes the markup for iOS a styled tab-bar. Remove the // to include
-        //include('./iosTabbar.php');
+        //include('./snippets/iosTabbar.php');
 ?>
     <div class="container">
 
 <?php // this includes the footer
-//include('./header.php');?>
+//include('./snippets/header.php');?>
         <br>
         <div class="well">
             <h1>End of Prototype</h1>
@@ -58,13 +56,13 @@ include('./headTag.php');?>
       <hr>
 
 <?php // this includes the footer
-include('./footer.php');?>
+include('./snippets/footer.php');?>
 
     </div> <!-- /container -->
 
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-<?php include ('protostrap_javascripts.php');?>
+<?php include ('./meta_javascripts.php');?>
   </body>
 </html>
