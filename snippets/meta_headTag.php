9978a56adba64792590d986cea2da690cdb9942b
Memi Beltrame <memi@lessamess.ch>
2014-04-16 13:44:45 +0200

    move to snippets

---
+++ snippets/meta_headTag.php
@@ -0,0 +1,25 @@
+    <meta charset="utf-8"><meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+    <!-- Le styles -->
+    <link href="./assets/css/bootstrap.min.css?time=<?php time();?>" rel="stylesheet">
+    <!-- Uncomment the following line if having the full width of the screen is needed
+    <link href="./assets/css/extendBootstrapGrid.min.css?time=<?php time();?>" rel="stylesheet">
+    -->
+    <link rel="stylesheet" href="./assets/css/add2home.css">
+    <link href="./assets/css/fontawesome.css?time=<?php time();?>" rel="stylesheet">
+    <link href="./assets/css/protostrap.css?time=<?php time();?>" rel="stylesheet">
+    <link href="./assets/css/main.css?time=<?php time();?>" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="/assets/ico/favicon.ico?<?php echo time() ;?>">
+    <link rel="apple-touch-icon" sizes="114x114" href="/assets/ico/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/assets/ico/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/assets/ico/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" href="/assets/ico/apple-touch-icon-57x57.png">
