07523da40e4485a59792a7b8e26c0ec42ff3fa6c
Memi Beltrame <memi@lessamess.ch>
2014-08-18 11:16:15 +0200

    add datepicker translations

--- assets/js/protostrap.js
+++ assets/js/protostrap.js
@@ -14,15 +14,50 @@
         }, false);
 
     }
-    function init_datepicker(){
-        $('.input-group.date, .date').datepicker({
-            language: "de",
-            orientation: "auto left",
-            format: "dd.mm.yyyy",
-            autoclose: true,
-            todayHighlight: true
-        });
+
+    // Define Datepicker Languages
+    $.fn.datepicker.dates['de'] = {
+        days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
+        daysShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam", "Son"],
+        daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+        months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+        monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+        today: "Heute",
+        clear: "Löschen",
+        weekStart: 1,
+        format: "dd.mm.yyyy"
     }
+    $.fn.datepicker.dates['fr'] = {
+        days: ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"],
+        daysShort: ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam", "Dim"],
+        daysMin: ["D", "L", "Ma", "Me", "J", "V", "S", "D"],
+        months: ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"],
+        monthsShort: ["Jan", "Fév", "Mar", "Avr", "Mai", "Jui", "Jul", "Aou", "Sep", "Oct", "Nov", "Déc"],
+        today: "Aujourd'hui",
+        clear: "Effacer",
+        weekStart: 1,
+        format: "dd/mm/yyyy"
+    };
+    $.fn.datepicker.dates['it'] = {
+        days: ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica"],
+        daysShort: ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab", "Dom"],
+        daysMin: ["Do", "Lu", "Ma", "Me", "Gi", "Ve", "Sa", "Do"],
+        months: ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"],
+        monthsShort: ["Gen", "Feb", "Mar", "Apr", "Mag", "Giu", "Lug", "Ago", "Set", "Ott", "Nov", "Dic"],
+        today: "Oggi",
+        clear: "Cancella",
+        weekStart: 1,
+        format: "dd/mm/yyyy"
+    };
+    $.fn.datepicker.dates['es'] = {
+        days: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"],
+        daysShort: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb", "Dom"],
+        daysMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sa", "Do"],
+        months: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+        monthsShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+        today: "Hoy"
+    };
+
 
     function init_select(){
         $('.selectpicker').selectpicker();
