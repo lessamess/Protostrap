bdebb89e1337434a79eb2fba231a10465a83ae7b
Memi Beltrame <memi@lessamess.ch>
2014-08-20 11:37:36 +0200

    add android icon and code

--- snippets/meta_headTag.php
+++ snippets/meta_headTag.php
@@ -1,7 +1,9 @@
     <?php if(empty($pathToAssets)){
         $pathToAssets = "";
         } ?>
-    <meta charset="utf-8"><meta name="apple-mobile-web-app-capable" content="yes">
+    <meta charset="utf-8">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
 
@@ -20,10 +22,24 @@
     <![endif]-->
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="/assets/ico/favicon.ico?<?php echo time() ;?>">
-    <link rel="apple-touch-icon" sizes="114x114" href="/assets/ico/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/assets/ico/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/assets/ico/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" href="/assets/ico/apple-touch-icon-57x57.png">
+    <link rel="shortcut icon" href="<?php echo $pathToAssets ;?>assets/ico/favicon.ico?<?php echo time() ;?>">
+
+    <!-- For iPad with high-resolution Retina display running iOS ≥ 7: -->
+    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo $pathToAssets ;?>assets/ico/apple-touch-icon-152x152.png">
+    <!-- For iPad with high-resolution Retina display running iOS ≤ 6: -->
+    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo $pathToAssets ;?>assets/ico/apple-touch-icon-144x144.png">
+    <!-- For iPhone with high-resolution Retina display running iOS ≥ 7: -->
+    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo $pathToAssets ;?>assets/ico/apple-touch-icon-120x120.png">
+    <!-- For iPhone with high-resolution Retina display running iOS ≤ 6: -->
+    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo $pathToAssets ;?>assets/ico/apple-touch-icon-114x114.png">
+    <!-- For the iPad mini and the first- and second-generation iPad on iOS ≥ 7: -->
+    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo $pathToAssets ;?>assets/ico/apple-touch-icon-76x76.png">
+    <!-- For the iPad mini and the first- and second-generation iPad on iOS ≤ 6: -->
+    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo $pathToAssets ;?>assets/ico/apple-touch-icon-72x72.png">
+    <!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+    <link rel="apple-touch-icon" href="<?php echo $pathToAssets ;?>assets/ico/apple-touch-icon.png">
+    <!-- For Android -->
+    <link rel="icon" sizes="196x196" href="android-196.png">
+
 
 
