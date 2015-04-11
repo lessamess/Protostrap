9ae4684ababc67c3ce297a20d54221c9f7af08e2
Memi Beltrame <memi@lessamess.ch>
2015-04-11 22:11:10 +0200

    update loginform

--- snippets/loginForm.php
+++ snippets/loginForm.php
@@ -1,31 +1,51 @@
-            <?php if(empty($loginaction)){
-                    $loginaction = "";
-                } ?>
+<?php
+if(empty($loginaction)){
+        $loginaction = "";
+}
+$loginFirstUser = $users[1][$config['loginWith']];
+?>
 
-            <div class="row">
-                <span class="col-xs-12 col-md-12">
-                    <h3>Sign in</h3>
-                </span>
+<div class="row">
+    <span class="col-xs-12 col-md-12">
+        <h3>Sign in</h3>
+    </span>
+</div>
+<div class="row">
+    <span class="col-xs-12 col-md-3">
+
+
+
+        <form action='<?php echo $loginaction ;?>' method='post' id="loginform">
+            <div class="form-group">
+                <label for="login">Username</label>
+                <input id="login" name='login' class='form-control' type='text' placeholder='Username' autofocus>
             </div>
-            <div class="row">
-                <span class="col-xs-12 col-md-3">
-                    <form action='<?php echo $loginaction ;?>' method='post' id="loginform">
-                        <div class="form-group"><input id="login" name='login' class='form-control' type='text' placeholder='Username' autofocus></div>
-                        <div class="input-group">
-                              <input type="password" class="form-control" name="" placeholder="Password" >
-                              <span class="input-group-addon passwordToggle"><i class="fa fa-square-o"></i> Show</span>
-                        </div>
 
-                        <br>
-
-                        <button type='submit' class='btn-block btn btn-primary'>Sign in</button>
-                    </form>
-                </span>
-                <span class="col-xs-12 col-md-offset-1 col-md-6">
-                <strong>User-Roles</strong><br>
-                    Click the following to log in as:<br>
-                    <?php foreach($users as $key => $user){ ?>
-                        <strong><a href="javascript:void(0);" class="loginUserselection" data-key="<?php echo  $user['email'];?>"><?php echo  $roles[$user['role']]['name']; ?></a></strong><br>
-                        <?php }?>
-                </span>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <div class="input-group">
+                    <input type="password" class="form-control" name="" placeholder="Password" >
+                    <span class="input-group-addon passwordToggle"><i class="fa fa-square-o"></i> Show</span>
+                </div>
             </div>
+
+            <button type='button' class="btn-block btn btn-primary loginFirstUser" data-key="<?php echo $loginFirstUser ;?>">Sign in</button>
+        </form>
+    </span>
+    <span class="col-xs-12 col-md-offset-1 col-md-6">
+    <strong>User-Roles</strong><br>
+        Click the following to sign in as:<br>
+        <?php foreach($users as $key => $user){ ?>
+            <strong><a href="javascript:void(0);" class="loginUser" data-key="<?php echo  $user[$config['loginWith']];?>"><?php echo  $roles[$user['role']]['name']; ?></a></strong><br>
+            <?php }
+
+        echo "<br>";
+
+        if ($loggedIn){
+            echo "You are currently signed in as:<br> ". $activeUser['fullName']." <br>Role: ".$roles[$userrole]['name']." <br>";?>
+            <a href="?logout=true">Log out</a>
+        <?php } else {
+            echo "You are currently not signed in.";
+        }?>
+    </span>
+</div>
