4666c08abafb813afbfd4eb4c1c703a87607c082
Memi Beltrame <memi@lessamess.ch>
2015-04-11 22:12:05 +0200

    cosmetics

--- functions_controller.php
+++ functions_controller.php
@@ -1,42 +1,14 @@
 <?php
 
-function figureFormat($num, $dez = 2){
+function figureFormat($num, $decimals = 2){
     $num = floatval($num);
-   return number_format($num, $dez, '.', '&rsquo;');
+   return number_format($num, $decimals, '.', '&rsquo;');
 }
 
 // Order stuff via uasort($array, 'itemOrder');
 function itemOrder($a, $b) {
 
-    // Change 'orderKey' to whatever Index your array should be ordered by
-    return $a['orderIndex'] > $b['orderIndex'] ? 1 : -1;
+    // Change 'name'-key to whatever Index your array should be ordered by
+    return $a['order'] > $b['order'] ? 1 : -1;
 
-}
-
-function box($text, $class="info",$icon="inherit", $id="", $dismiss = true ){
-    if ($icon == "inherit") {
-        switch ($class) {
-            case 'success':
-                $icon = "check";
-                break;
-            case 'danger':
-                $icon = "warning";
-                break;
-
-            default:
-                $icon = "info-circle";
-                break;
-        }
-    }
-    echo "<div id=\"" . $id . "\" class=\"alert alert-". $class ."\">";
-    if($dismiss){
-        echo "<button class='close' data-dismiss='alert'  type='button'>×</button>";
-    }
-    echo "<ul class=\"fa-ul\">
-          <li style='width:96%'>
-            <i class=\"fa fa-" . $icon . " fa-lg fa-li\"></i>
-            " . $text . "
-          </li>
-        </ul>
-      </div>";
 }
