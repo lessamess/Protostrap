456621652b69786a6b359860ed26b8bfea3a63fc
Memi Beltrame <memi@lessamess.ch>
2015-04-11 09:08:31 +0200

    add box function

--- functions_controller.php
+++ functions_controller.php
@@ -10,5 +10,33 @@ function itemOrder($a, $b) {
 
     // Change 'orderKey' to whatever Index your array should be ordered by
     return $a['orderIndex'] > $b['orderIndex'] ? 1 : -1;
-    
+
+}
+
+function box($text, $class="info",$icon="inherit", $id="", $dismiss = true ){
+    if ($icon == "inherit") {
+        switch ($class) {
+            case 'success':
+                $icon = "check";
+                break;
+            case 'danger':
+                $icon = "warning";
+                break;
+
+            default:
+                $icon = "info-circle";
+                break;
+        }
+    }
+    echo "<div id=\"" . $id . "\" class=\"alert alert-". $class ."\">";
+    if($dismiss){
+        echo "<button class='close' data-dismiss='alert'  type='button'>×</button>";
+    }
+    echo "<ul class=\"fa-ul\">
+          <li style='width:96%'>
+            <i class=\"fa fa-" . $icon . " fa-lg fa-li\"></i>
+            " . $text . "
+          </li>
+        </ul>
+      </div>";
 }
