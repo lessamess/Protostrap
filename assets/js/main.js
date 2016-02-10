85e3fae7998b7b46c1f679bae0e8eda3956229cb
Memi Beltrame <memi@lessamess.ch>
2016-02-10 22:47:07 +0100

    add typeahead example

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,16 +11,64 @@
             todayHighlight: true
         });
 
-        //livesearch example
-        $('.typeahead').typeahead({
-              source: ['foo bar','foo fighters','a fools errand','football'],
-              limit: 10,
-              updater: function(item){
-                // Do things when selected
-                // console.log(item);
-                return item;
-              }
-        });
+        //LIVESEARCH EXAMPLE in 3 Parts
+                // Documentation: https://github.com/twitter/typeahead.js/blob/master/doc/jquery_typeahead.md
+                // Doc-Passage on datasets: https://github.com/twitter/typeahead.js/blob/master/doc/jquery_typeahead.md#datasets
+
+            // Part 1: the Matcher Function
+            // LEAVE ALONE
+            var substringMatcher = function(strs) {
+              return function findMatches(q, cb) {
+                var matches, substringRegex;
+
+                // an array that will be populated with substring matches
+                matches = [];
+
+                // regex used to determine if a string contains the substring `q`
+                substrRegex = new RegExp(q, 'i');
+
+                // iterate through the pool of strings and for any string that
+                // contains the substring `q`, add it to the `matches` array
+                $.each(strs, function(i, str) {
+                  if(str.name != undefined ){
+                      if (substrRegex.test(str.name)) {
+                        matches.push(str);
+                      }
+                  } else {
+                    if (substrRegex.test(str)) {
+                        matches.push(str);
+                      }
+                  }
+                });
+
+                cb(matches);
+              };
+            };
+
+            // Part 2: the Source Data
+            var sourcedata = ['foo bar','foo fighters ticket','a fools errand','football'];
+
+
+            // Part 3: the Livesearch itself
+
+            //
+            $('.typeahead').typeahead({
+                      highlight: true,
+                      minLength: 1
+                    },
+                    {
+                      name: 'mytypeahead',
+                      source: substringMatcher(sourcedata),
+                      templates: {
+                        notFound: '<p class="tt-noentries" ><strong>No entries found</strong></p>',
+                        footer: '<p class="tt-footer" ><button class="btn btn-default">Show all results</button></p>',
+                        suggestion: function(data){
+                              return '<p class="topborder"><strong>' + data + '</strong>  <span class="pull-right">$' +  Math.floor((Math.random() * 100) + 1)  + '.99</span></p>';
+                            }
+                        }
+                    });
+
+        // END OF TYPEAHEAD EXAMPLE
 
         $(document).bind('keydown','alt+r', function(){
             var url = window.location.href;
