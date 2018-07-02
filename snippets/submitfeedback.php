94bdbf4c3ceefdb5f48f92c6ba872aa049a693a9
Memi Beltrame <memi@lessamess.ch>
2018-07-02 09:26:47 +0200

    form feedback template

---
+++ snippets/submitfeedback.php
@@ -0,0 +1,20 @@
+<div class="submitfeedback align-center">
+    <br>
+    <i class="fa fa-spinner fa-3x fa-spin"></i>
+    <br>
+</div>
+<div class="submitfeedback hide align-left">
+    <br>
+    <div class="flexbox">
+        <div class="fixed-size-60 align-center" >
+            <span class="fa-stack fa-lg">
+              <i class="fa fa-circle fa-stack-2x text-success"></i>
+              <i class="fa fa-check fa-stack-1x fa-inverse"></i>
+            </span>
+        </div>
+        <div class="flex-1" >
+            <h4><?php echo $feedback_title ;?></h4>
+            <?php echo $feedback_text ;?>
+        </div>
+    </div>
+</div>
